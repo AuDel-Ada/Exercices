928f85c722dbeb9461b1b0bec8d33161d49b7998
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2022-01-21 23:33:51 +0100

    Ok for the stars, but not for the blanks

--- Christmas_Tree.js
+++ Christmas_Tree.js
@@ -3,7 +3,6 @@ function sapin(n) {
   // Create Top of the Christmas Tree :
   let star = ["  ","+"];
   let top = [" ","/*\\"];
-  let line1 = ["/*","*","*\\"];
 
   function createTop (array) {
     for (i=1; i<n ; i++) {
@@ -14,19 +13,17 @@ function sapin(n) {
 
   createTop(star);
   createTop(top);
-  createTop(line1);
 
   // Create Middle of the Christmas Tree : 
-  let line2 = ["/*","*","*\\"];
+  let line = ["/","*","\\"];
+  let place = 1;
 
-  if (n == 1) {
-    return
-  } else {
-    for (i=1; i<=n ; i++){
-      line2.splice(n,0,"*");
-    };
-    console.log(line2.join(""))
+  for (i=1; i<=n ; i++){
+    line.unshift(" ");
+    place ++;
+    line.splice(place,0,"**");
+    console.log(line.join(""));
   };
 };
 
-sapin(2);
+sapin(3);
