a8ae179f9aa79c8dc94c2558a8939658d2c04478
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2022-01-22 00:56:58 +0100

    First level OK !!

--- Christmas_Tree.js
+++ Christmas_Tree.js
@@ -7,23 +7,30 @@ function sapin(n) {
   function createTop (array) {
     for (i=1; i<n ; i++) {
       array.unshift(" ");
-    }
+    };
     console.log(array.join(""));
-  }
+  };
 
   createTop(star);
   createTop(top);
 
   // Create Middle of the Christmas Tree : 
   let line = ["/","*","\\"];
-  let place = 1;
+
+  /// To have enough blanks which will be
+  /// decremented in the next loop
+  for (i=1; i<=n ; i++) {
+    line.unshift(" ");
+  };
 
   for (i=1; i<=n ; i++){
-    line.unshift(" ");
-    place ++;
-    line.splice(place,0,"**");
+    line.shift(" ");
+    line.splice(n,0,"**");
     console.log(line.join(""));
   };
+
 };
 
-sapin(3);
+sapin(1);
+sapin(2);
+sapin(5);
