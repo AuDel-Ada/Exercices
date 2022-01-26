76ddb0fb9880e605c6c2769fd30987da57faf327
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2022-01-26 19:53:39 +0100

    Figure how to loop on the 2 sets of passwords

--- HERITAGE/LEVEL 3.py
+++ HERITAGE/LEVEL 3.py
@@ -1,10 +1,10 @@
 #LEVEL 3
+
+# Construct the password
 from itertools import chain, combinations
-import subprocess
 
 print("List of Password to try with tresor3 :")
 PasswordEnd = ["Linus", "docker", "if", "opéra", "468153084652"]
-
 
 def powerset(list_name):
     s = list(list_name)
@@ -15,12 +15,28 @@ for world in powerset(PasswordEnd):
     set1 = "91Grace"+""
     for element in world:
         set1 += element
-    print(set1)
     
     set2 = "91Margaret"+""
     for element in world:
         set2 += element
-    print(set2)
+        
+    print (type(set1))
+    Password = set1 + set2
+    print(Password)
 
-# Command line
-subprocess.Popen(['unrar', 'e', 'tresor3.rar'])
+# Command lines from script :
+
+# import pexpect
+# import time
+
+# child = pexpect.spawn ("unrar", ["e", "tresor3.rar"])
+
+# child.expect ("password")
+# time.sleep (0.1)
+# child.sendline (set1)
+# choices = child.expect (["incorrect", "xtracting"])
+# if choices == 0 :
+#     print ("Wrong password")
+# if choices == 1 :
+#     time.sleep (2)
+#     print ("Process completed")
